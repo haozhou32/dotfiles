f6e5d30dcce905869c34b0e8ce7c40dca6019d67
haozhou32 <zhouhao199102@gmail.com>
2026-01-04 21:05:25 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################### SKETCHYBAR ####################
+# install sketchybar
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# install dependencies
+brew install --cask sf-symbols
+brew install --cast font-sf-pro
+brew install jq
+brew install switchaudio-osx
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# start sketchybar
+brew services start sketchybar
